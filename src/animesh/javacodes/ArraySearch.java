ca023bb0708553ca109a16266ded382881203346
Animesh Dwivedi <animeshdwivedinwm@gmail.com>
2020-06-07 16:18:25 -0500

    Adding Binary Search

--- src/animesh/javacodes/ArraySearch.java
+++ src/animesh/javacodes/ArraySearch.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class ArraySearch {
 
+
 	public static void linearSearch(Integer numArray[], Integer n, Integer searchNum) {
 		int pos = -1;
 		for(int j = 0; j < n; j++) {
@@ -17,6 +18,24 @@ public class ArraySearch {
 			System.out.println("Number found at : " + pos);
 		}
 
+	}
+
+	public static void binarySearch(Integer numArray[], Integer n, Integer searchNum) {
+		int beg = 0;
+		int mid = (0 + n) / 2;
+		int end = n-1;
+		while(beg <= end) {
+			if(numArray[mid] == searchNum) {
+				System.out.println("Number found at : " + mid);
+				beg++;
+				end--;
+			} else if(numArray[mid] > searchNum) {
+				end = mid - 1;
+			} else {
+				beg = mid + 1;
+			}
+			mid = (beg + end) / 2;
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -32,7 +51,10 @@ public class ArraySearch {
 		}
 		System.out.println("Enter Element to be searched");
 		int searchNum = sc.nextInt();
+		System.out.println("Linear Search");
 		linearSearch(num, n, searchNum);
+		System.out.println("Binary Search");
+		binarySearch(num, n, searchNum);
 		sc.close();
 	}
 
